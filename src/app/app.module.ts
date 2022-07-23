88865c18bb7c86072d8dafe8f87ba896dcb80333
Patricia Mulaudzi <32956237+Patricious@users.noreply.github.com>
2022-07-23 12:19:57 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AadInvoiceComponent } from './aad-invoice/aad-invoice.component';
+import { AddInvoiceComponent } from './components/add-invoice/add-invoice.component';
+import { HomeComponent } from './components/home/home.component';
+import { IncoiceComponent } from './components/incoice/incoice.component';
+import { InvoicesListComponent } from './components/invoices-list/invoices-list.component';
+import { DialogComponent } from './components/dialog/dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AadInvoiceComponent,
+    AddInvoiceComponent,
+    HomeComponent,
+    IncoiceComponent,
+    InvoicesListComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/incoice/incoice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-incoice',
+  templateUrl: './incoice.component.html',
+  styleUrls: ['./incoice.component.css']
+})
+export class IncoiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
